04b74ace1de9e037546532958d623de26f5db407
lipilian <liuhong2@illinois.edu>
2021-01-28 10:50:58 -0600

    no temp saving

--- LiuHongProcess.py
+++ LiuHongProcess.py
@@ -12,28 +12,41 @@ import math
 from tqdm import tqdm
 import os
 # %% parameter need to set
-VideoPath = "./NoEle/LEDlight+DMM pair test.avi"
+VideoPath = "./calibration/1um_triangle_pulse_2.avi"
 ImagePath = "./tempImage/"
-estimateFeatureSize = 13 # must be odd numer
+estimateFeatureSize = 19 # must be odd numer
 minMass = 200 # calculate the integrate minMass intensity
-SavePath = VideoPath.split('/')[-1].split('.')[0]
-
+SavePath = VideoPath.split('/')[-1].split('.avi')[0]
+if not os.path.exists(SavePath):
+    os.mkdir(SavePath)
 # %% read the image and save them as frame image and transfer them from rgb image
 # to grayscale
+'''
 vidcap = cv2.VideoCapture(VideoPath)
 success,image = vidcap.read()
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 count = 0
 success = True
-while success and count < 2000:
+while success and count < 3000:
     cv2.imwrite(ImagePath + "frame%d.png" % count, image)     # save frame as PNG file
     success,image = vidcap.read()
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # print ('Read a new frame: ', success)
     count += 1
+'''
+# %%
+frames = pims.PyAVReaderIndexed(VideoPath)
+
+@pims.pipeline
+def as_grey(frame):
+    red = frame[:, :, 0]
+    green = frame[:, :, 1]
+    blue = frame[:, :, 2]
+    return 0.2125 * red + 0.7154 * green + 0.0721 * blue
+frames = as_grey(frames)
 
 # %% test with parameter (First run to find the min Mass)
-frames = pims.open(ImagePath + '*.png')
+# frames = pims.open(ImagePath + '*.png')
 f = tp.locate(frames[1000], estimateFeatureSize)
 # plot the mass diagram to check the orginal distribution of the mass histogram
 fig, ax = plt.subplots()
@@ -49,11 +62,11 @@ tp.subpx_bias(f)
 # %% locate feature in all frames
 f = tp.batch(frames, estimateFeatureSize, minmass = minMass)
 # %%
-t = tp.link(f, 50, memory=5)
+t = tp.link(f, 20, memory=5)
 # %%
 plt.figure()
 tp.annotate(t[t['frame'] == 0], frames[0]);
-t1 = tp.filter_stubs(t, 100)
+t1 = tp.filter_stubs(t, 10)
 
 # %% check raw figure(optional)
 tp.plot_traj(t1)
@@ -62,12 +75,12 @@ tp.plot_traj(t1)
 #%% sort and filter traj to avoid not moving particles
 
 Ntrajs = np.max(np.array(t1['particle'])) + 1
-minMoveDistance = 800
+minMoveDistance = 400
 print('there are %s trajectories' % Ntrajs)
 t2 = t1[0:0]
 for i in range(Ntrajs):
     tNew = t1[t1['particle']==i]
-    if(len(tNew) < 100):
+    if(len(tNew) < 30):
         continue
     #distData = tp.motion.msd(tNew,1,1,len(tNew))
     #dist = distData.iloc[-1,:]['msd']
@@ -81,6 +94,7 @@ for i in range(Ntrajs):
         t2 = t2.append(tNew)
         
 # %%
+'''
 fig = plt.figure()
 plt.imshow(frames[0],cmap = 'gray')
 LineData = pd.read_csv('./Line.csv')
@@ -95,7 +109,7 @@ x1 = np.array([startx1, endx1]); y =np.array([0,h])
 x2 = np.array([startx2, endx2])
 plt.plot(x1,y,'-',linewidth = 2, color='red')
 plt.plot(x2,y,'-',linewidth = 2, color='red')
-
+'''
 # %% calculate line functions
 '''
 coefficients1 = np.polyfit(x1, y, 1)
@@ -114,21 +128,24 @@ for i in range(len(frames)):
     intensity.append(ValidIntensity)
 '''
 # %%
+'''
 intensity = []
 for i in tqdm(range(len(frames))):
     curFrame = frames[i]#[:,int(min(x1[0],x2[0])):int(max(x1[1],x2[1]))]
     intensity.append(np.average(np.average(curFrame)))
-
+'''
 # %%
+'''
 f = plt.figure()
 plt.plot(intensity)
 for i in range(len(frames)):
-    if(intensity[i]>22):
+    if(intensity[i]>100):
         break
 plt.title('First flashing frame is ' + str(i) + '. So time is ' + str(i) + '/70.54 s')
-f.savefig('LEDtime.jpg')  
+f.savefig(SavePath +'/LEDtime.jpg')  
 df = DataFrame(intensity, columns=['intensity'])
-df.to_csv('./intensity.csv')
+df.to_csv(SavePath + '/intensity.csv')
+'''
 # %%
 
 k,ax = plt.subplots(1, figsize=(6,4))
@@ -136,14 +153,8 @@ k,ax = plt.subplots(1, figsize=(6,4))
 #ax.plot(x2,y,'-',linewidth = 2, color='red')
 tp.plot_traj(t2)
 
-k.savefig('images.jpg')
-t2.to_csv('./pointsData.csv')
+k.savefig(SavePath + '/images.jpg')
+t2.to_csv(SavePath + '/pointsData.csv')
 
-
-# %% locate
-distance = tp.motion.emsd(t2,1,1,len(t2))
-
-# %%
-t2
 
 # %%
